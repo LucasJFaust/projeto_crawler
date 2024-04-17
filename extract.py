565a7c5816d17b226054f9b1704a35d788f94b7a
LucasJFaust <lucasjfm.eng@gmail.com>
2024-04-17 17:05:20 -0300

    3 commit

--- extract.py
+++ extract.py
@@ -1,4 +1,4 @@
-import requests
+import requests # type: ignore
 
 url = "https://www.mercadolivre.com.br/ofertas?container_id=MLB779362-9#origin=scut&filter_position=1&is_recommended_domain=false"
 
@@ -10,3 +10,23 @@ data = requests.get(url).text
 
 with open('mercadolivre.html', 'w', encoding = 'utf-8') as f:
     f.write()
+
+# Retorna titulos dos produtos nas tags
+
+def get_itens_title(doc):
+    title_tags = doc.find_all('p', class_ = 'promotion-item_title')
+    titles = []
+    for tags in title_tags:
+        titles.append(tags.text)
+
+    return titles
+
+# Retorna preços dos prosutos
+
+def get_itens_price(doc):
+    price_tags = doc.find_all('p', class_ = 'promotion-item_title')
+    prices = []
+    for tags in price_tags_tags:
+        prices.append(tags.text)
+
+    return prices
